73f643eb5cb411094bbbf66965ee6eba949f2633
jdaychayma <77275724+jdaychayma@users.noreply.github.com>
2021-02-15 10:09:06 +0100

    Update App.js

--- React-Hooks-Movie-App-main/src/App.js
+++ React-Hooks-Movie-App-main/src/App.js
@@ -7,8 +7,8 @@ import NavBar from './Components/NavBar';
 function App() {
 
   const [movieList, setMovieList] = useState([
-    { title: 'Ahmed', posterUrl: 'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg', description: 'bla bla bla bla', rate: "3" },
-    { title: 'achref', posterUrl: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2', description: 'bla bla bla bla', rate: "3" }
+    { title: 'Chayma', posterUrl: 'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg', description: 'bla bla bla bla', rate: "3" },
+    { title: 'Senda', posterUrl: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2', description: 'bla bla bla bla', rate: "3" }
   ])
 
   const [newTitle, setNewTitle] = useState("");
